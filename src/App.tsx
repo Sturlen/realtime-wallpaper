f99c318aabb490c2d5f959205b077e3ed75b8e1e
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-06-26 14:55:23 +0200

    Improved styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,41 +7,41 @@ function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Realtime wallpaer Demo</h1>
-        <p>Show live sun progress</p>
-        <ul className="static-info">
-          <ul className="info-block">
-            <h2>Location</h2>
-            <p>Lat: </p><input type="number"></input>
-            <br/>
-            <p>Lon: </p><input type="number"></input>
+      <h1>Realtime wallpaper Demo</h1>
+      </header>
+        
+        <main className="content">
+          <ul className="static-info">
+            <ul className="info-block">
+              <h2>Location</h2>
+              <p>Lat: </p><input type="number"></input>
+              <br/>
+              <p>Lon: </p><input type="number"></input>
+            </ul>
+            <ul className="info-block">
+              <h2>Date</h2>
+              <p>Dawn at 00:00</p><input type="date"></input>
+              <br/>
+              <p>Dusk at 00:00</p>
+            </ul>
+            <ul className="info-block">
+              <h2>Sun times</h2>
+              <p>Dawn at 00:00</p>
+              <br/>
+              <p>Dusk at 00:00</p>
+            </ul>
           </ul>
           <ul className="info-block">
-            <h2>Date</h2>
-            <p>Dawn at 00:00</p><input type="date"></input>
-            <br/>
-            <p>Dusk at 00:00</p>
-          </ul>
-          <ul className="info-block">
-            <h2>Sun times</h2>
-            <p>Dawn at 00:00</p>
-            <br/>
-            <p>Dusk at 00:00</p>
-          </ul>
-        </ul>
-        
-        
-        
-        <ul className="info-block">
           <li><p>Sun altitude:</p><code>XX</code></li>
           <li><p>Sun azimuth:</p><code>XX</code></li>
         </ul>
-        <div className="time-control">
+        </main>
+
+      <footer className="time-control">
           <h3>Time</h3>
-          <input type="range" step={1} min={0} max={86400}/>
+          <input type="range" step={1} min={0} max={86400} defaultValue={0}/>
           <h3>00:00:00</h3>
-        </div>        
-      </header>
+        </footer>
     </div>
   )
 }
