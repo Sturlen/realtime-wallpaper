c0fddf5c696cbabd27b880803923c8647abee68c
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-06-26 14:34:52 +0200

    Reworked test page

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,46 @@
-import React, { useState } from "react"
-import logo from "./logo.svg"
+import React from "react"
+
 import "./App.css"
-import SunCalc from "suncalc"
-import { useTimeOfDay } from "./TimeOfDay"
-
-const date = new Date(Date.now())
-
-const lat = 58.946047
-
-const long = 5.694283
-
-const testlat = 71.0598
-
-const testlong = 25.75195
-
-const times = SunCalc.getTimes(date, lat, long)
 
 function App(): JSX.Element {
-  const [state, actions] = useTimeOfDay()
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{
-            transform: `translateX(${state.progress * 100 - 50}vw)`,
-          }}
-        />
-        <p>{`Sunrise: ${times.sunrise.toLocaleTimeString("no-nb", {
-          hour12: false,
-        })}`}</p>
-        <p>{`Sunset: ${times.sunset.toLocaleTimeString("no-nb", {
-          hour12: false,
-        })}`}</p>
-        <p>
-          {"pos " +
-            SunCalc.getPosition(new Date(Date.now()), 58.946047, 5.694283)
-              .altitude}
-        </p>
-        <p>{"progress " + state.progress}</p>
-        <input
-          type="time"
-          step={0.0000000000000000000000000001}
-          onChange={(e): void => actions.update(new Date())}
-        ></input>
+        <h1>Realtime wallpaer Demo</h1>
+        <p>Show live sun progress</p>
+        <ul className="static-info">
+          <ul className="info-block">
+            <h2>Location</h2>
+            <p>Lat: </p><input type="number"></input>
+            <br/>
+            <p>Lon: </p><input type="number"></input>
+          </ul>
+          <ul className="info-block">
+            <h2>Date</h2>
+            <p>Dawn at 00:00</p><input type="date"></input>
+            <br/>
+            <p>Dusk at 00:00</p>
+          </ul>
+          <ul className="info-block">
+            <h2>Sun times</h2>
+            <p>Dawn at 00:00</p>
+            <br/>
+            <p>Dusk at 00:00</p>
+          </ul>
+        </ul>
+        
+        
+        
+        <ul className="info-block">
+          <li><p>Sun altitude:</p><code>XX</code></li>
+          <li><p>Sun azimuth:</p><code>XX</code></li>
+        </ul>
+        <div className="time-control">
+          <h3>Time</h3>
+          <input type="range" step={1} min={0} max={86400}/>
+          <h3>00:00:00</h3>
+        </div>        
       </header>
     </div>
   )
