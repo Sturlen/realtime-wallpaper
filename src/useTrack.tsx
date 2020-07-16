0af088f689759d44141744c52ed64d95e0b57b71
Sturle Nesvik Spetland <43607012+Sturlen@users.noreply.github.com>
2020-07-16 19:35:55 +0200

    Removing unused files

--- src/useTrack.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react"
-import { SunContext } from "./suncontext"
-
-export default function useTrack(track: string): [number] {
-  const { azimuth, postition } = useContext(SunContext)
-  return [1]
-}
